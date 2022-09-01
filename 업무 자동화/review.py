034f7ea85ca66f62a5f2a063270eff80cf261631
강병하 <76998855+TacticalCoders@users.noreply.github.com>
2022-09-01 19:54:00 +0900

    상품 첫 번째 리뷰 텍스트 추출

--- 업무 자동화/review.py
+++ 업무 자동화/review.py
@@ -4,12 +4,11 @@ from selenium.webdriver.common.by import By
 import time
 import subprocess
 
-p1 = subprocess.Popen('chrome.exe --remote-debugging-port=9222 --user-data-dir="C:/ChromeTEMP"', cwd="C:/Program Files/Google/Chrome/Application/" ,shell=True)
-print(p1)
-
-time.sleep(2)
 
 product_url = input('상품 url을 입력하세요:')
+
+p1 = subprocess.Popen('chrome.exe --remote-debugging-port=9222 --user-data-dir="C:/ChromeTEMP"', cwd="C:/Program Files/Google/Chrome/Application/" ,shell=True)
+print(p1)
 
 chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
@@ -22,6 +21,10 @@ time.sleep(2)
 
 driver.execute_script("window.scrollBy(0, 1000);") # 스크롤
 
-driver.find_element(By.XPATH,'//*[@id="btfTab"]/ul[1]/li[2]').click()
+driver.find_element(By.XPATH,'//*[@id="btfTab"]/ul[1]/li[2]').click() # 상품평 버튼
 
-reviews = []
+time.sleep(2)
+
+review = driver.find_element(By.XPATH, '//*[@id="btfTab"]/ul[2]/li[2]/div/div[6]/section[4]/article[1]/div[4]/div').text
+
+print(review)
